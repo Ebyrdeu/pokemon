099a1fdb0f45dad52ee06cef60e442c057e3d860
Maxim Khnykin <ebyrdeu.khnykin@gmail.com>
2022-11-23 18:11:07 +0100

    ### Added --- logger to trpc --- mantine
    ### Remove
    --- tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,24 @@
-import type {AppType} from "next/app";
+import {AppType} from "next/app";
+import Head from "next/head";
+import {MantineProvider} from "@mantine/core";
 import {trpc} from "@/utils/trpc";
 
 const MyApp: AppType = ({Component, pageProps}) => {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <Head>
+          <title>Page title</title>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+        </Head>
+
+        <MantineProvider
+            withGlobalStyles
+            withNormalizeCSS
+            theme={{colorScheme: "dark"}}
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </>
+  );
 };
 export default trpc.withTRPC(MyApp);
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,20 @@
 import type {NextPage} from "next";
 import {trpc} from "@/utils/trpc";
+import {useState} from "react";
+import {Button, Text} from "@mantine/core";
 
 
 
 const Home: NextPage = () => {
-
-  const pokemon = trpc.pokemon.useQuery({id: 1});
+  const [state, setState] = useState(1)
+  const pokemon = trpc.pokemon.useQuery({id: state});
   if (!pokemon.data?.status) {
     return <div>Loading...</div>;
   }
   return (
       <div>
-        <p>{pokemon.data?.data.name}</p>
+        <Button onClick={() => setState(prevState => prevState + 1)}>New Pokemon</Button>
+        <Text fz={'xl'}>{pokemon.data?.data.name}</Text>
       </div>
   );
 };
